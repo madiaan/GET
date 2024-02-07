046113581f540a4ef9aa330841d417acf76f7fc2
madiaan <55086237+madiaan@users.noreply.github.com>
2024-02-07 14:00:31 +0100

    Add files via upload

---
+++ values.js
@@ -0,0 +1,86 @@
+//PERCENTILE VALUES
+var zeroPrcnt = 5725;
+//var zeroPrcntShow = document.getElementById('zeroPerc');
+var tenPrcnt = 7351;
+var lowerPrcnt = 7790;
+var medianPrcnt = 8219;
+var highPrcnt = 8503;
+var ninetyPrcnt = 8681;
+var orangePrcnt = 8749;
+var topPrcnt = 8866;
+var peakPrcnt = 9019;
+
+//FIGHT DURATION
+
+
+
+
+//var allPercs = (function() {
+
+
+//CALCULATE PERCENTILE FUNCTIONS
+//ZERO PERCENTILE
+//function isItZero() {
+//    if(roundedvalue < tenPrcnt) {
+//        yourBarse.innerHTML= /*HTML*/ `
+//        <div id="zeroPerc">0<br/>Percentile</div> `
+//        }
+//}
+////TENTH PERCENTILE
+//function isItTen() {
+//    if(roundedvalue < lowerPrcnt && roundedvalue > tenPrcnt) {
+//        yourBarse.innerHTML= /*HTML*/ `
+//        <div id="tenPerc">10<br/>Percentile</div> `
+//    }
+//}
+////LOWER PERCENTILE
+//function isItLower() {
+//    if(roundedvalue < medianPrcnt && roundedvalue > lowerPrcnt) {
+//        yourBarse.innerHTML= /*HTML*/ `
+//        <div id="lowerPerc">25<br/>Percentile</div> `
+//    }
+//}
+////MEDIAN PERCENTILE
+//function isItMedian() {
+//    if(roundedvalue < highPrcnt && roundedvalue > medianPrcnt) {
+//        yourBarse.innerHTML= /*HTML*/ `
+//        <div id="medianPerc">50<br/>Percentile</div> `
+//    }
+//}
+////HIGH PERCENTILE
+//function isItHigh() {
+//    if(roundedvalue < ninetyPrcnt && roundedvalue > highPrcnt) {
+//        yourBarse.innerHTML= /*HTML*/ `
+//        <div id="highPerc">75<br/>Percentile</div> `
+//    }
+//}
+////NINETY PERCENTILE
+//function isItNinety() {
+//    if(roundedvalue < orangePrcnt && roundedvalue > ninetyPrcnt) {
+//        yourBarse.innerHTML= /*HTML*/ `
+//        <div id="ninetyPerc">90<br/>Percentile</div> `
+//    }
+//}
+////ORANGE PERCENTILE
+//function isItOrange() {
+//    if(roundedvalue < topPrcnt && roundedvalue > orangePrcnt) {
+//        yourBarse.innerHTML= /*HTML*/ `
+//        <div id="orangePerc">95<br/>Percentile</div> `
+//    }
+//}
+////TOP PERCENTILE
+//function isItTop() {
+//    if(roundedvalue < peakPrcnt && roundedvalue > topPrcnt) {
+//        yourBarse.innerHTML= /*HTML*/ `
+//        <div id="topPerc">99<br/>Percentile</div> `
+//    }
+//}
+////PEAK PERCENTILE
+//function isItPeak() {
+//    if(roundedvalue > peakPrcnt) {
+//        yourBarse.innerHTML= /*HTML*/ `
+//        <div id="peakPerc">100<br/>Percentile</div> `
+//    }
+//}
+//return {isItZero, isItTen, isItLower, isItMedian, isItHigh, isItNinety, isItOrange, isItTop, isItPeak}
+//})();
