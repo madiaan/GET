616fa8854b9914b6b5828305e211280cc206e2a0
madiaan <55086237+madiaan@users.noreply.github.com>
2024-04-08 11:46:23 +0200

    Add files via upload

---
+++ zmodeller copy/model2.js
@@ -0,0 +1,84 @@
+const model2 = {
+    app: {
+        page: null
+    },
+    registeredUsers: [
+        {user: 'martin', password: 'hell'},
+        {user: 'thor', password: 'hihi123'},
+        {user: 'håvard', password: 'hemelig'},
+    ],
+    question: [
+        {
+            q: '1. Hvor ofte har du problemer med å avslutte en oppgave etter at de interessante delene er unnagjort?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '2. Hvor ofte er det vanskelig for deg å få orden på ting når du skal utføre en oppgave som krever organisering?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '3. Hvor ofte har du problemer med å huske avtaler eller forpliktelser?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '4. Når du har en oppgave som krever at du tenker nøye igjennom det du skal gjøre, hvor ofte unngår eller utsetter du å begynne på den?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '5. Hvor ofte sitter du og fikler med noe når du må sitte lenge i ro?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '6. Hvor ofte føler du deg overdrevet aktiv og tvunget til å gjøre noe, som om du var drevet av en indre motor?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '7. Hvor ofte gjør du slurvefeil når du må jobbe med en kjedelig eller vanskelig oppgave?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '8. Hvor ofte har du problemer med å holde oppmerksomheten oppe når du gjør kjedelig eller ensformig arbeid?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '9. Hvor ofte har du vansker med å konsentrere deg om hva folk sier, selv når de snakker direkte til deg?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '10. Hvor ofte har du vanskeligheter med å finne igjen ting hjemme eller på jobb?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '11. Hvor ofte blir du distrahert av aktiviteter eller lyder rundt deg?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '12. Hvor ofte forlater du plassen din på møter eller i andre situasjoner der det forventes at du blir sittende?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '13. Hvor ofte føler du deg rastløs eller urolig i kroppen?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '14. Hvor ofte har du vanskelig for å ta det med ro og slappe av når du har tid for deg selv?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '15. Hvor ofte opplever du å snakke for mye i sosiale sammenhenger?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '16. Hvor ofte opplever du at du fullfører setninger for andre før de rekker å fullføre dem selv?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '17. Hvor ofte har du problemer med å vente på tur i situasjoner der dette er nødvendig?',
+            a: [false, false, false, false, false]
+        },
+        {
+            q: '18. Hvor ofte avbryter du andre når de holder på med noe?',
+            a: [false, false, false, false, false]
+        },
+    ],
+};
